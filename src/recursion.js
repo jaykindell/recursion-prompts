1dff1586d68bc389357760c64c23defb3ab14fc3
Jay Kindell <jaykindell@gmail.com>
2018-10-16 13:05:35 -0500

    nominal change

--- src/recursion.js
+++ src/recursion.js
@@ -4,6 +4,7 @@
 // denoted by n!, is the product of all positive integers less than or equal to n.
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
+// nominal change
 var factorial = function (n, total, i = n) {
     if (total === undefined) { total = 1 }
     if (i < 0) {
